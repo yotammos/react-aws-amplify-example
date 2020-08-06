121b20c03a304e4cf6311fea3d91993fb5dce23d
Yotam Moss <yotam.mos@gmail.com>
2020-08-06 10:47:06 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from v2!</h1>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
